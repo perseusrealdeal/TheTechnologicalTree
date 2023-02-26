2559ed8ecb48b532ff717c0f8341b555720b18ad
Mikhail <mzhigulin@gmail.com>
2023-02-26 13:26:17 +0700

    ADDED: standalone one
    This commit goes with standalone one included.

---
+++ XcodeTemplateProjectSingle.swift
@@ -0,0 +1,15 @@
+//
+//  XcodeTemplateProjectSingle.swift
+//  XcodeTemplateProject
+//
+//  Created by Mikhail Zhigulin in 7531.
+//
+//  Copyright © 7531 Mikhail Zhigulin of Novosibirsk.
+//
+//  Licensed under the MIT license. See LICENSE file.
+//  All rights reserved.
+//
+
+struct XcodeTemplateProject {
+    var text = "Hello, World!"
+}
