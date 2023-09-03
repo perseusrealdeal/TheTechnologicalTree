ac38b8ffb2c14181a641dc4a8580b3b600f8caa2
Mikhail <mzhigulin@gmail.com>
2023-09-03 10:08:27 +0700

    UPDATED: license notice

--- XcodeTemplateProject/BusinessContent/MainViewController.swift
+++ XcodeTemplateProject/BusinessContent/MainViewController.swift
@@ -4,10 +4,12 @@
 //
 //  Created by Mikhail Zhigulin in 7531.
 //
-//  Copyright © 7531 Mikhail Zhigulin of Novosibirsk.
+//  Copyright © 7531 - 7532 Mikhail Zhigulin of Novosibirsk
 //
-//  Licensed under the special license. See LICENSE file.
-//  All rights reserved.
+//  The year starts from the creation of the world according to a Slavic calendar.
+//  September, the 1st of Slavic year.
+//
+//  See LICENSE for details. All rights reserved.
 //
 
 import UIKit
