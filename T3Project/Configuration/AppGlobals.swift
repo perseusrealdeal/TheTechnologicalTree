5e4c88a8f1f92aebcaee828ce055a514f142f7d9
Mikhail <mzhigulin@gmail.com>
2024-02-03 06:15:39 +0700

    ADDED: common system relative functions

--- T3Project/Configuration/AppGlobals.swift
+++ T3Project/Configuration/AppGlobals.swift
@@ -12,9 +12,24 @@
 //  See LICENSE for details. All rights reserved.
 //
 
-import Foundation
+import Cocoa
 
 struct AppGlobals {
+
+    // MARK: - Constants
+
+    static var systemOptionsAppName: String? {
+
+        var calculatedTitle: String?
+
+        if #available(macOS 10.14, *) {
+            calculatedTitle = "System Settings.app"
+        } else {
+            calculatedTitle = "System Preferences.app"
+        }
+
+        return calculatedTitle
+    }
 
     // MARK: - System Services
 
@@ -22,4 +37,57 @@ struct AppGlobals {
     static let nc = NotificationCenter.default
 
     // MARK: - Custom Services
+
+
+    // MARK: - Init
+
+    init() {
+
+        log.message("[AppGlobals].\(#function)")
+
+        // Init custom services here.
+
+    }
+
+    // MARK: - Common system relative functions
+
+    static func clickWebLink(string link: String) {
+
+        log.message("[\(type(of: self))].\(#function)")
+
+        guard let url = NSURL(string: link) as URL? else {
+            log.message("[\(type(of: self))].\(#function)", .error)
+            return
+        }
+
+        _ = NSWorkspace.shared.open(url) ?
+            log.message("[\(type(of: self))].\(#function) - Web browser opened.") :
+            log.message("[\(type(of: self))].\(#function) - Web browser not opened.")
+    }
+
+    static func makeTheAppRun(_ name: String?) {
+
+        /* One way to open System options app */
+
+        let mger = FileManager.default
+
+        guard
+            let theAppName = name,
+            let pathFirst = mger.urls(for: .applicationDirectory, in: .systemDomainMask).first
+        else {
+            return
+        }
+
+        /* Another way to open System options app */
+        // guard let pathURL = URL(string: AppGlobals.openSystemApp) else { return }
+
+        NSWorkspace.shared.open(pathFirst.appendingPathComponent(theAppName))
+    }
+
+    static func quitTheApp() {
+
+        // Instructions before quit.
+
+        app.terminate(appDelegate)
+    }
 }
